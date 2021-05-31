51e7904aa059d24a9c0f0fb7bc6b4bcbdfe08d74
chhiring90 <cgtamang90@gmail.com>
2021-05-31 22:28:15 +0545

    everest: updated avatar component

--- everest/src/components/Avatar.js
+++ everest/src/components/Avatar.js
@@ -1,6 +1,34 @@
-const avatar = ({ imageSrc, alt }) => (
-    <figure className="">
-        <img src={imageSrc} alt={alt} />
-    </figure>)
+const avatar = ({ imageSrc, alt, size }) => {
+    let avatar;
+
+    const classes = ['rounded-full mx-auto w-24 h-24 overflow-hidden'];
+
+    switch (size) {
+        case 'large':
+            avatar = (
+                <figure className={classes}>
+                    <img src={imageSrc} alt={alt} />
+                </figure>
+            );
+            break;
+        case 'medium':
+            avatar = (
+                <figure className={classes}>
+                    <img src={imageSrc} alt={alt} />
+                </figure>
+            );
+            break;
+        case 'small':
+            avatar = (
+                <figure className={classes}>
+                    <img src={imageSrc} alt={alt} />
+                </figure>
+            );
+            break;
+        default: break;
+    }
+
+    return avatar;
+};
 
 export default avatar;
--- everest/src/components/Navbar.js
+++ everest/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Avatar from './Avatar';
+import DefaultAvatar from '../assets/images/jabber-default.svg';
+
 const Navbar = () => {
     return (
-        <nav className="max-w-nav w-full shadow-nav h-screen bg-white">
-
+        <nav className="max-w-nav w-full shadow-nav h-screen bg-white pt-12">
+            <Avatar imageSrc={DefaultAvatar} size="large" />
         </nav>
     )
 }
